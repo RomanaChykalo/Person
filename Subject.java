ec4b74ac2813aeb3f875ae0a2cdb4c9ffe703127
Romana Chykalo <romo4kachukalo@gmail.com>
2018-09-27 15:42:09 +0300

    Subject

--- Subject.java
+++ Subject.java
@@ -2,21 +2,8 @@ package remarkedHomeWork2;
 	public enum Subject{
 		ALGEBRA("algebra"), HISTORY("history"), BIOLOGI("biologi"), ENGLISH("english");
 	String subject;
-	private Subject(String subject){
+	Subject(String subject){
 		this.subject = subject;
 	}
-	public static Subject getSubject(String subject){
-		  switch(subject){
-		    case "algebra":return ALGEBRA;
-		    case "history":return HISTORY;
-		    case "biologi":return BIOLOGI;
-		  }
-		return null;
-	}
-	public static void main(String[] args){
-		System.out.println(getSubject("history"));
-		System.out.println(getSubject("history"));
-		System.out.println(getSubject("algebra"));
-		System.out.println(getSubject("algebra"));
-	}
+	
 }
